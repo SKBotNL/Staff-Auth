9d166f1658b3861a3e91a525afa78dd5d0df8382
Sven <skbotnl@disroot.org>
2026-05-23 19:56:47 +0200

    Cache Minecraft profiles

--- src/main/kotlin/net/trueog/staffauth/client/MinecraftClient.kt
+++ src/main/kotlin/net/trueog/staffauth/client/MinecraftClient.kt
@@ -1,15 +1,20 @@
 package net.trueog.staffauth.client
 
+import io.micronaut.cache.annotation.CacheConfig
+import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
 import net.trueog.staffauth.client.dto.MinecraftProfileDto
 import java.util.*
 
+@CacheConfig("minecraft-profiles")
 @Client("https://api.mojang.com/")
 interface MinecraftClient {
+    @Cacheable
     @Get("users/profiles/minecraft/{username}")
     suspend fun getByUsername(username: String): MinecraftProfileDto?
 
+    @Cacheable
     @Get("user/profile/{uuid}")
     suspend fun getByUuid(uuid: UUID): MinecraftProfileDto?
 }
